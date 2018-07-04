900ce9b617cba1b4a0f76710a5452e58a5805208
Sean M <sean@arrowburn.co.nz>
2018-07-05 09:50:32 +1200

    updated search component and added links to each individual recipe

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Query } from "react-apollo";
+import { SEARCH_RECIPES } from "../../queries";
+import { Link } from 'react-router-dom';
 
 const Search = () => (
-    <div>Search</div>
-)
+  <Query query={SEARCH_RECIPES} variables={{ searchTerm: "" }}>
+    {({ data, loading, error }) => {
+        if(loading) return <div>loading</div>;
+        if(error) return <div>Error</div>;
+        console.log(data)
+      return (
+        <div className="App">
+          <input type="search" />
+          <ul>
+              {data.searchRecipes.map(recipe => (
+                  <li key={recipe._id}>
+                    <Link to={`/recipes/${recipe._id}`}><h4>{recipe.name}</h4></Link>
+                    <p>{recipe.likes}</p>
+                  </li>
+              ))}
+          </ul>
+        </div>
+      );
+    }}
+  </Query>
+);
 
 export default Search;
