60bf65259a2840df35c03152ad6ba2cfaa0b57bf
Sean M <sean@arrowburn.co.nz>
2018-07-05 10:55:23 +1200

    updated search Component

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -1,29 +1,43 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { ApolloConsumer } from "react-apollo";
 import { SEARCH_RECIPES } from "../../queries";
-import { Link } from 'react-router-dom';
+import SearchItem from './SearchItem'
 
-const Search = () => (
-  <Query query={SEARCH_RECIPES} variables={{ searchTerm: "" }}>
-    {({ data, loading, error }) => {
-        if(loading) return <div>loading</div>;
-        if(error) return <div>Error</div>;
-        console.log(data)
-      return (
-        <div className="App">
-          <input type="search" />
-          <ul>
-              {data.searchRecipes.map(recipe => (
-                  <li key={recipe._id}>
-                    <Link to={`/recipes/${recipe._id}`}><h4>{recipe.name}</h4></Link>
-                    <p>{recipe.likes}</p>
-                  </li>
-              ))}
-          </ul>
-        </div>
-      );
-    }}
-  </Query>
-);
+class Search extends React.Component {
+    state = {
+        searchResults: []
+    }
+
+    handleChange = ({ searchRecipes }) => {
+        this.setState({
+            searchResults: searchRecipes
+        })
+    }
+    render() {
+        const { searchResults } = this.state;
+        return(
+            <ApolloConsumer>
+            {client => (
+            <div className="App">
+              <input onChange={async (event) => {
+                  event.persist();
+                  const { data } = await client.query({
+                      query: SEARCH_RECIPES,
+                      variables: { searchTerm: event.target.value }
+                  })
+                  this.handleChange(data);
+              }} type="search" placeholder="Search for Recipes"/>
+              <ul>
+                  {searchResults.map(recipe => (
+                      <SearchItem key={recipe._id} {...recipe}/>
+                  ))}
+              </ul>
+            </div>
+            )}
+        </ApolloConsumer>
+        )
+    }
+
+}
 
 export default Search;
