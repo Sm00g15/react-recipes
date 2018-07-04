dd5d2450629a62eae4b77702264a555c888c12e8
Sean M <sean@arrowburn.co.nz>
2018-07-04 13:54:47 +1200

    setup addRecipe form

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const AddRecipe = () => (
-    <div>Recipe</div>
+    <div className="App">
+        <h2 className="App">Add Recipe</h2>
+        <form className="form" action="">
+            <input type="text" name="name" onChange={this.handleChange} placeholder="Recipe Name"/>
+            <select name="category" onChange={this.handleChange}>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Lunch">Lunch</option>
+                <option value="Dinner">Dinner</option>
+                <option value="Snack">Snack</option>
+            </select>
+            <input type="text" name="description" placeholder="Add Description" onChange={this.handleChange}/>
+            <textarea name="instructions" placeholder="Add Instructions" onChange={this.handleChange} id="" cols="60" rows="30"></textarea>
+            <button type="submit" className="button-primary">Submit</button>
+        </form>
+    </div>
 )
 
 export default AddRecipe;
