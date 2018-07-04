acd5f6ca6a8cb27c5f68ac541c89fa3d8c8288f9
Sean M <sean@arrowburn.co.nz>
2018-07-04 16:42:42 +1200

    implementing add recipe form:

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { ADD_RECIPE } from '../../queries';
+import Error from '../Error';
 
 class AddRecipe extends Component {
     state = {
@@ -6,8 +9,14 @@ class AddRecipe extends Component {
         instructions: '',
         category: 'Breakfast',
         description: '',
-        username: ''
+        username: '', 
     };
+
+    componentDidMount() {
+        this.setState({
+            username: this.props.session.getCurrentUser.username
+        })
+    }
 
     handleChange = event => {
         const { name, value } = event.target
@@ -16,25 +25,46 @@ class AddRecipe extends Component {
         })
     }
 
+    validateForm = () => {
+        const { name, category, instructions, description, username } = this.state;
+        const isInvalid = !name || !category || !instructions || !description;
+        return isInvalid;
+    }
+
+    handleSubmit = (event, addRecipe) => {
+        event.preventDefault();
+        addRecipe().then(({data}) => {
+            console.log(data);
+        })
+    }
+
+    
     render() { 
         const { name, category, instructions, description, username } = this.state;
         return ( 
-            <div className="App">
-            <h2 className="App">Add Recipe</h2>
-            <form className="form" action="">
-                <input value={name} type="text" name="name" onChange={this.handleChange} placeholder="Recipe Name"/>
-                <select value={category} name="category" onChange={this.handleChange}>
-                    <option value="Breakfast">Breakfast</option>
-                    <option value="Lunch">Lunch</option>
-                    <option value="Dinner">Dinner</option>
-                    <option value="Snack">Snack</option>
-                </select>
-                <input value={description} type="text" name="description" placeholder="Add Description" onChange={this.handleChange}/>
-                <textarea value={instructions} name="instructions" placeholder="Add Instructions" onChange={this.handleChange} id="" cols="60" rows="30"></textarea>
-                <button type="submit" className="button-primary">Submit</button>
-            </form>
-        </div>
-         )
+        <Mutation mutation={ADD_RECIPE} variables={{ name, category, instructions, description, username }}>
+            {( addRecipe, { data, loading, error }) => {
+                return(
+                    <div className="App">
+                    <h2 className="App">Add Recipe</h2>
+                    <form className="form" action="" onSubmit={event => this.handleSubmit(event, addRecipe)}>
+                        <input value={name} type="text" name="name" onChange={this.handleChange} placeholder="Recipe Name"/>
+                        <select value={category} name="category" onChange={this.handleChange}>
+                            <option value="Breakfast">Breakfast</option>
+                            <option value="Lunch">Lunch</option>
+                            <option value="Dinner">Dinner</option>
+                            <option value="Snack">Snack</option>
+                        </select>
+                        <input value={description} type="text" name="description" placeholder="Add Description" onChange={this.handleChange}/>
+                        <textarea value={instructions} name="instructions" placeholder="Add Instructions" onChange={this.handleChange} id="" cols="60" rows="30"></textarea>
+                        <button disabled={loading || this.validateForm()} type="submit" className="button-primary">Submit</button>
+                        {error && <Error error={error} /> }
+                    </form>
+                </div>
+                );
+            }}
+        </Mutation>
+        );
     }
 }
  
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -27,6 +27,19 @@ export const GET_RECIPE = gql`
 `
 // Recipes Mutations
 
+export const ADD_RECIPE = gql`
+    mutation($name: String!, $description: String!, $category: String!, $instructions: String!, $username: String) {
+        addRecipe(name: $name, description: $description, category: $category, instructions: $instructions, username: $username) {
+           _id
+           name
+           category
+           description
+           instructions
+           username
+        }
+    }
+`
+
 // User Queries
 
 export const GET_CURRENT_USER = gql`
