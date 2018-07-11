0d77a553aef5a49875ac8fc3a2f3e8cdf5f6c265
Sean M <sean@arrowburn.co.nz>
2018-07-12 10:18:19 +1200

    implemented likeRecipe functionality

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -36,6 +36,15 @@ export const SEARCH_RECIPES = gql`
 `
 // Recipes Mutations
 
+export const LIKE_RECIPE = gql`
+    mutation($_id: ID!, $username: String!) {
+        likeRecipe(_id: $_id, username: $username) {
+            _id
+            likes
+        }
+    }
+`;
+
 export const ADD_RECIPE = gql`
     mutation($name: String!, $description: String!, $category: String!, $instructions: String!, $username: String) {
         addRecipe(name: $name, description: $description, category: $category, instructions: $instructions, username: $username) {
