9881c62e083462373442097fa0831f7c7f2bfd18
Sean M <sean@arrowburn.co.nz>
2018-07-13 12:50:05 +1200

    updates

--- client/src/components/Recipe/LikeRecipe.js
+++ client/src/components/Recipe/LikeRecipe.js
@@ -5,19 +5,34 @@ import { LIKE_RECIPE } from '../../queries';
 
 class LikeRecipe extends Component {
   state = {
+    liked: false,
     username: ""
   };
   componentDidMount() {
     if (this.props.session.getCurrentUser) {
-      const { username } = this.props.session.getCurrentUser;
-      console.log(username);
-      this.setState({ username });
+      const { username, favorites } = this.props.session.getCurrentUser;
+      const { _id } = this.props;
+      const prevLiked = favorites.findIndex(favorite => favorite._id === _id) > -1;
+      this.setState({ liked: prevLiked, username });
     }
   }
+  handleClick = likeRecipe => {
+      this.setState(prevState => ({
+          liked: !prevState.liked
+      }),
+    () => this.handleLike(likeRecipe)
+    )
+  }
+
   handleLike = (likeRecipe) => {
-    likeRecipe().then(({ data }) => {
+    if(this.state.liked){
+    likeRecipe().then(async ({ data }) => {
       console.log(data);
+      await this.props.refetch();
     });
+}   else {
+    console.log('unlike');
+}
   };
   render() {
     const { username } = this.state;
@@ -25,7 +40,9 @@ class LikeRecipe extends Component {
     return (
       <Mutation mutation={LIKE_RECIPE} variables={{ _id, username }}>
         {likeRecipe =>
-          username && <button onClick={() => this.handleLike(likeRecipe)}>Like</button>
+          username && <button onClick={() => this.handleClick(likeRecipe)}>
+        {this.state.liked ? 'Unlike': 'Like'}
+          </button>
         }
       </Mutation>
     );
